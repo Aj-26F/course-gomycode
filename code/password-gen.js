dc08f00b06ea24e2a1935a43d4e228eb93ef7830
Aj-26F <121810775+Aj-26F@users.noreply.github.com>
2023-01-02 14:48:35 +0000

    Delete code directory

--- code/password-gen.js
+++
@@ -1,7 +0,0 @@
-var gepwd= require("generate-password");
-var password= gepwd.generate({
-    length: 14,
-    numbers: true,
-    symbols: true,
-});
-console.log("le mot de passe gérnéré est: "+password);
--- code/serveur-node.js
+++
@@ -1,8 +0,0 @@
-// creation du serveur
-var http = require("http"); 
-http.createServer(function(req, res){ //req c est l'ensemble des informations liées aux serveur
-    res.writeHead(200,{'Content-Type': 'text/html'});  //res permet d'interagir avec le serveur
-    res.write('<h1> hello node!!!!</h1>\n');
-    res.end()
-
-}).listen(3000);
